d31029490473abffdeaa10c1234e09c6191a90bd
786haider <haiderhussain536@gmail.com>
2024-12-10 09:34:38 -0800

    third

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
    <div>
     <h1>Home</h1>
+    <p className="bg-black text-white text-2xl">contniously work on this web an updated it time by time and complete my hakathon project and make it live as soon as possible.</p>
    </div>
   );
 }
