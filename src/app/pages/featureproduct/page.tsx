34c3a653d5aaf14b2a55bf94a3b8393d853a49fd
786haider <haiderhussain536@gmail.com>
2024-12-28 03:19:17 +0500

    adding card

--- src/app/pages/featureproduct/page.tsx
+++ src/app/pages/featureproduct/page.tsx
@@ -1,11 +1,21 @@
+import Card1 from "./featureproductCards/card1";
+
 
 export default function FeatureProduct(){
     return(
         <div>
-            <h1 style={{color:'rgba(26, 11, 91, 1)',width:'367px',height:'42px'}}>
-
+            <h1 style={{color:'rgba(26, 11, 91, 1)',height:'42px',top:'1015px',left:'777px',fontSize:'42px',lineHeight:'49.22px'}}
+            className="flex justify-center items-center font-extrabold font-serif w-auto mt-20">
+            Featured Products
             </h1>
-           <h1 className="text-xl text-black"> Feature Product Page</h1>
-        </div>
+            <div className="flex gap-6 ml-10 mt-5">
+                <Card1/>
+               
+                </div> 
+       
+
+           </div>
+
+        
     );
 }
