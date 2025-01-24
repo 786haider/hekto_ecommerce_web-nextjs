389d7609d9d49832926dff06932a90cfee9b5d1b
786haider <haiderhussain536@gmail.com>
2025-01-25 01:46:19 +0500

    finilizing cards

--- src/app/pages/featureproduct/featureproductCards/card1.tsx
+++ src/app/pages/featureproduct/featureproductCards/card1.tsx
@@ -1,51 +1,169 @@
-import Image from "next/image";
-import chairImage from "../../../../../public/chairfeature.png";
+// import Image from "next/image";
+// import chairImage from "../../../../../public/chairfeature.png";
+// import Link from "next/link";
+// export default function Card1() {
+//   return (
+//     <div className="flex-col border rounded-md shadow-xl ">
+//      <Link href={'/'} >
+//      <div
+//         style={{
+//           width: "270px",
+//           height: "361px",
+//           top: "1105px",
+//           left: "376px",
+//         }}
+//       >
+//         <div
+//           style={{
+//             width: "270px",
+//             height: "236px",
+//             backgroundColor: "rgba(246, 247, 251, 1)",
+//           }}
+//         >
+//           <Image
+//             src={chairImage}
+//             alt="chairimage"
+//             style={{
+//               width: "178px",
+//               height: "178px",
+//               top: "46px",
+//               left: "40px",
+//             }}
+//             className="flex justify-center items-center ml-auto mr-auto mt-10 mb-auto "
+//           />
+//         </div>
+//         <div
+//           style={{ backgroundColor: "rgba(255, 255, 255, 1)", width: "270px" }}
+//         >
+//           <h1
+//             style={{
+//               height: "22px",
+//               top: "251px",
+//               left: "71px",
+//               color: "rgba(251, 46, 134, 1)",
+//               fontWeight: 700,
+//               lineHeight: "21px",
+//               fontSize: "18px",
+//             }}
+//             className="flex justify-center items-center w-auto mt-3"
+//           >
+//             Cantilever chair
+//           </h1>
+//           <div
+//             className="flex justify-center items-center gap-1 ml-28 mt-3"
+//             style={{
+//               width: "52px",
+//               height: "4px",
+//               top: "285px",
+//               left: "110px",
+//               borderRadius: "10px",
+//             }}
+//           >
+//             <div
+//               style={{
+//                 width: "14px",
+//                 height: "4px",
+//                 top: "285px",
+//                 left: "110px",
+//                 borderRadius: "10px",
+//                 background: "rgba(5, 230, 183, 1)",
+//               }}
+//             ></div>
+//             <div
+//               style={{
+//                 width: "14px",
+//                 height: "4px",
+//                 top: "285px",
+//                 left: "110px",
+//                 borderRadius: "10px",
+//                 background: "rgba(247, 1, 168, 1)",
+//               }}
+//             ></div>
+//             <div
+//               style={{
+//                 width: "14px",
+//                 height: "4px",
+//                 top: "285px",
+//                 left: "110px",
+//                 borderRadius: "10px",
+//                 background: "rgba(0, 0, 157, 1)",
+//               }}
+//             ></div>
+//           </div>
+//           <p
+//             style={{
+//               color: "rgba(21, 24, 117, 1)",
+//               fontSize: "14px",
+//               left: "92px",
+//             }}
+//             className="ml-20 mt-3"
+//           >
+//             Code - Y523201
+//           </p>
+//           <p
+//             style={{
+//               color: "rgba(21, 24, 117, 1)",
+//               fontSize: "14px",
+//               lineHeight: "16.8px",
+//             }}
+//             className="mt-3 ml-28"
+//           >
+//             $42.00
+//           </p>
+//         </div>
+//       </div>
+//      </Link>
+//     </div>
+//   );
+// }
+
 import Link from "next/link";
+import chairImage from "../../../../../public/chairfeature.png";
+import Image from "next/image";
 export default function Card1() {
   return (
-    <div className="flex-col border rounded-md shadow-xl ">
-     <Link href={'/'} >
-     <div
-        style={{
-          width: "270px",
-          height: "361px",
-          top: "1105px",
-          left: "376px",
-        }}
-      >
+    <div
+      style={{ width: "270px", height: "361px", top: "1105px", left: "975px" }}
+      className="border rounded-md shadow-lg"
+    >
+      <Link href={"/"}>
         <div
           style={{
             width: "270px",
             height: "236px",
             backgroundColor: "rgba(246, 247, 251, 1)",
           }}
+          className="flex justify-center"
         >
           <Image
             src={chairImage}
-            alt="chairimage"
+            alt="Chair"
+            width={216}
+            height={151}
             style={{
-              width: "178px",
-              height: "178px",
-              top: "46px",
-              left: "40px",
+              top: "41px",
+              left: "41px",
+              width:'216px',
+              height:'151px'
+              
             }}
-            className="flex justify-center items-center ml-auto mr-auto "
+            className="mt-auto mb-auto"
           />
         </div>
         <div
-          style={{ backgroundColor: "rgba(255, 255, 255, 1)", width: "270px" }}
+          style={{ width: "270px", backgroundColor: "rgba(255, 255, 255, 1)" }}
         >
           <h1
             style={{
               height: "22px",
               top: "251px",
               left: "71px",
-              color: "rgba(251, 46, 134, 1)",
               fontWeight: 700,
               lineHeight: "21px",
               fontSize: "18px",
+              color: "rgba(251, 46, 134, 1)",
             }}
-            className="flex justify-center items-center w-auto mt-3"
+            className="flex justify-center items-center w-auto  "
           >
             Cantilever chair
           </h1>
@@ -111,8 +229,7 @@ export default function Card1() {
             $42.00
           </p>
         </div>
-      </div>
-     </Link>
+      </Link>
     </div>
   );
 }
--- src/app/pages/featureproduct/page.tsx
+++ src/app/pages/featureproduct/page.tsx
@@ -17,6 +17,13 @@ export default function FeatureProduct(){
                <Card3/>
                <Card4/>
                 </div> 
+                <div className="flex gap-1 mt-5 ml-auto mr-auto" style={{width:'91px',height:'4px',top:'1519px',left:"915px",borderRadius:'10px'}}>
+                    <div style={{width:'24px',backgroundColor:'rgba(251, 46, 134, 1)',height:'4px',left:'915px',borderRadius:'10px'}}> </div>
+                    <div style={{width:'16px',backgroundColor:'rgba(254, 186, 215, 1)',height:'4px',left:'915px',borderRadius:'10px'}}></div>
+                    <div style={{width:'16px',backgroundColor:'rgba(254, 186, 215, 1)',height:'4px',left:'915px',borderRadius:'10px'}}></div>
+                    <div style={{width:'16px',backgroundColor:'rgba(254, 186, 215, 1)',height:'4px',left:'915px',borderRadius:'10px'}}></div>
+
+                </div>
        
 
            </div>
